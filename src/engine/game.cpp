829980aaccdf88b99ac84f732489d2ac7d7e7011
Alex Frutkin <frutkinalexander@gmail.com>
2021-09-10 13:50:25 -0700

    game

---
+++ src/engine/game.cpp
@@ -0,0 +1,20 @@
+/*
+    Copyright 2021 Alex Frutkin
+    
+    This file is part of yummymines.
+
+    yummymines is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    yummymines is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with yummymines.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
